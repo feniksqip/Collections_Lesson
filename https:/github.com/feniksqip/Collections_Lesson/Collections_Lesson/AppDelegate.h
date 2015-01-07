5a4edb872222cb755d71c1bb9b0ef3b59796f1ed
Admin <admin@Mac-Admin.local>
2015-01-07 23:03:23 +0300

    Create a Project.

---
+++ https:/github.com/feniksqip/Collections_Lesson/Collections_Lesson/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Collections_Lesson
+//
+//  Created by Admin on 07.01.15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
